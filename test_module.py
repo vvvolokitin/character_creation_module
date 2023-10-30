d55a94e0fd8366096f017debf368e8b1aeea269d
Никита Волокитин <n.a.volokitin@yandex.ru>
2023-10-30 19:58:22 +0500

    code accordnig to PEP8

--- test_module.py
+++ test_module.py
@@ -9,5 +9,6 @@ files_list = ['main.py', 'readme.md']
 
 def test_program():
     for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
-
+        assert filename in dir_files, f'''
+            Файл `{filename}` не найден в корне репозитория
+            '''
